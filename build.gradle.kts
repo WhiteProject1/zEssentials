a174e773fc0f0f481d15eb076f5feb060e15025e
Maxence <simon.maxence@hotmail.com>
2026-02-23 12:22:19 +0100

    :memo: Update to last zmenu build

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ allprojects {
 
     dependencies {
 //        compileOnly("fr.maxlego08.menu:zmenu-api:1.1.0.0")
-        compileOnly(files("libs/zMenu-1.1.0.9.jar"))
+        compileOnly(files("libs/zMenu-1.1.1.0.jar"))
 
         compileOnly("fr.maxlego08.sarah:sarah:1.20")
         compileOnly("com.tcoded:FoliaLib:0.5.1")
